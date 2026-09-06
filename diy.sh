2fa9317cf518d07a0729c864debaca5e74f762d4
as <as@asdeMac-Pro.local>
2026-09-06 13:01:03 +0800

    编译测试

--- diy.sh
+++ diy.sh
@@ -8,10 +8,10 @@
 set -e
 
 # 1. 修改默认 LAN IP（192.168.1.1 -> 10.0.0.1）
-sed -i 's/192.168.1.1/10.0.0.1/g' package/base-files/files/bin/config_generate
+sed -i 's/192.168.1.1/192.168.50.1/g' package/base-files/files/bin/config_generate
 
 # 2. 修改默认 root 密码为 root
-sed -i 's|^root:::|root:$1$wbroot$wOBLV0MeQc.zQXkjnygcq.:|' package/base-files/files/etc/shadow
+# sed -i 's|^root:::|root:$1$wbroot$wOBLV0MeQc.zQXkjnygcq.:|' package/base-files/files/etc/shadow
 
 # 3. 设置默认主题为 Argon
 mkdir -p files/etc/uci-defaults
